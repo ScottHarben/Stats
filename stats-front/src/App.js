84b8674b5a03f903f9135544d94d8b83dbd6ad18
Scott Harben <scottharben@gmail.com>
2022-04-03 22:21:10 -0400

    monkeys

--- stats-front/src/App.js
+++ stats-front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello world!</code>
+          <code>No more monkeys</code>
         </p>
         <a
           className="App-link"
