48c27efb08f8e08302d392df98fc8cac3dd06789
Scott Harben <scottharben@gmail.com>
2022-05-21 21:40:02 -0400

    comment out dotnev 2

--- stats-back/index.js
+++ stats-back/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config({ path: '../.env' });
+//require("dotenv").config({ path: '../.env' });
 
 const port = process.env.PORT;
 const host = process.env.VIRTUAL_HOST;
