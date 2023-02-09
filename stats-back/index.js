76d7dff8aaf2884f811b34ca24178901a4df76d6
Scott Harben <scottharben@gmail.com>
2023-02-09 08:23:08 -0500

    cors

--- stats-back/index.js
+++ stats-back/index.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://playthefade.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const { apiRouter } = require('./api');
 
